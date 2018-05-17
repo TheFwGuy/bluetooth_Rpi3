d6f95982e88b9f5d6c3e89aeb423dc419e369ba7
TheFwGuy <thefwguy@apkh.org>
2018-05-17 20:07:08 +0000

    Bring BT into thread - problems handling exceptions

--- BLTest.py
+++ BLTest.py
@@ -16,6 +16,8 @@ import subprocess as sp
 
 # Global variables
 
+server_socket=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+
 # Make list of button value, text, and backlight color.
 buttons = ( (LCD.SELECT, 'Select', (1,1,1)),
             (LCD.LEFT,   'Left'  , (1,0,0)),
@@ -29,16 +31,21 @@ buttons = ( (LCD.SELECT, 'Select', (1,1,1)),
 # 2 -> connected
 state = 0
 
-# Flag to informa about data read
-isData = False
+# Data TX/RX related variables
+isDataIn = False
+isDataOut = False
+dataIn = "  "
+dataOut = "  "
 
 # Define functions
 
 def waitBTthread():
    global state
    global server_socket
-   global client_socket
-   global address
+   global isDataIn
+   global isDataOut
+   global dataIn
+   global dataOut
 
    while True:
       if state == 1:
@@ -46,53 +53,51 @@ def waitBTthread():
          # print "Start wait ..."
          # NOTE ! The line below waits for a connection blocking the loop !
          client_socket,address = server_socket.accept()
-         # print "Accepted connection from ",address
-         # Set LCD plate color BLUE
-         lcd.set_color(0.0, 0.0, 1.0)
-         lcd.clear()
-         lcd.message('Connected\nBluetooth')
+         print "Accepted connection from ",address
          state = 2
 
+      if state == 2:
+         if isDataIn == False:
+            print "Wait RX"
+            try:
+               dataIn = client_socket.recv(1024)  # The function waits for characters !
+               isDataIn = True
+            except socket_error as serr:
+               print "Errno : %d" % serr.errno
+               if serr.errno != 11:
+                  client_socket.close()
+                  server_socket.close()
+                  state = 0
+                  isDataIn = False
+            except BluetoothError:
+               print "BT Erro ! Close connection"
+               client_socket.close()
+               server_socket.close()
+               state = 0
+               isDataIn = False
+            else:
+               print "Other exception !"
+#               isDataIn = False
+#               isDataOut = False
+#               client_socket.close()
+#               server_socket.close()
+#               state = 0
 
-def openConnection():
-   global server_socket
-   global client_socket
-   global address
+            if isDataIn:
+               print "Received: %s" % dataIn
+         
+               if (dataIn == 'q\n'):
+                  client_socket.close()
+                  server_socket.close()
+                  state = 0
+                  isDataIn = False
 
-   # Set up Bluettoth
-   # Restart Bluetooth and set for slave
-   os.popen('sudo hciconfig hci0 reset')
-   os.popen('sudo hciconfig hci0 piscan')
-
-   print "Bluetooth initialization - set as slave"
-
-   # Set up Bluetooth socket 
-   server_socket=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-
-   print "Use port_any : ", bluetooth.PORT_ANY 
-   server_socket.bind(("", bluetooth.PORT_ANY))
-   server_socket.listen(1)
-
-   t1 = threading.Thread(target= waitBTthread, args=())
-   if t1.is_alive() == False:
-      print "Start thread  waiting for connection"
-      t1.start()
+         if isDataOut:
+            print "Thread - attempt to send out data"
+            client_socket.send(dataOut)
+            isDataOut = False
 
 
-def closeConnection():
-   global server_socket
-   global client_socket
-   global address
-
-   client_socket.close()
-   server_socket.close()
-   print "Closed Bluetooth - disconnected"
-   # Set color LCD Red
-   lcd.set_color(1.0, 0.0, 0.0)
-   lcd.clear()
-   #lcd.message('Disconnected\nBluetooth')
-   lcd.message('IP WLAN:\n')
-   lcd.message(commands.getoutput('hostname -I'))
 
 # Initialize the LCD using the pins
 lcd = LCD.Adafruit_CharLCDPlate()
@@ -120,7 +125,8 @@ lcd.message(commands.getoutput('hostname -I'))
 # The main loop of the program check if the Select button is pressed.
 # If is pressed, the shutdown is issued
 
-isData = False;
+isDataIn = False
+isDataOut = False
 
 t1 = threading.Thread(target= waitBTthread, args=())
 
@@ -134,39 +140,58 @@ while True:
       lcd.clear()
       os.system("sudo poweroff")
 
+   # Color display management
+   if (state == 0):
+      lcd.set_color(1.0, 0.0, 0.0)
+      lcd.clear()
+      #lcd.message('Disconnected\nBluetooth')
+      lcd.message('IP WLAN:\n')
+      lcd.message(commands.getoutput('hostname -I'))
+   elif (state == 1):
+      lcd.set_color(0.0, 1.0, 0.0)
+   elif (state == 2):
+      lcd.set_color(0.0, 0.0, 1.0)
+   else:
+      lcd.set_color(0.0, 0.0, 0.0)
+
    # Bluetooth management
    if (state == 0):
-      print "Call openconnection"
-      openConnection()   # The function waits until a connection happens !
+      # Set up Bluettoth
+      # Restart Bluetooth and set for slave
+      os.popen('sudo hciconfig hci0 reset')
+      os.popen('sudo hciconfig hci0 piscan')
+
+      print "Bluetooth initialization - set as slave"
+
+      # Set up Bluetooth socket 
+      server_socket=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+
+      print "Use port_any : ", bluetooth.PORT_ANY 
+      server_socket.bind(("", bluetooth.PORT_ANY))
+      server_socket.listen(1)
+
+      t1 = threading.Thread(target= waitBTthread, args=())
+      if t1.is_alive() == False:
+         print "Start thread  waiting for connection"
+         t1.start()
       state = 1
 
 #   if (state == 1):
 #      print "."
 
    if (state == 2):
-      try:
-         data = client_socket.recv(10)  # The function waits for characters !
-         isData = True
-      except socket_error as serr:
-         isData = False
-         if serr.errno != 11:
-            print "Errno : %d" % serr.errno
-            closeConnection()
-            state = 0
+      if isDataIn:
+         print "Main : %s" % dataIn
+#         lcd.clear()
+#         lcd.message(dataIn)
+         isDataIn = False
 
-      if isData:
-         lcd.clear()
-         lcd.message(data)
-         print "Received: %s" % data
-         
-         if (data == 'q\n'):
-            closeConnection()
-            state = 0
-         isData = False
 
-      # Check if Down is pressed
-      if lcd.is_pressed(LCD.DOWN):
+   # Check if Down is pressed
+   if lcd.is_pressed(LCD.DOWN):
+      if isDataOut == False:
          # Button is pressed, send message
          print "Down button pressed - send message"
-         client_socket.send("Down")
+         dataOut = "Down"
+         isDataOut = True
 
